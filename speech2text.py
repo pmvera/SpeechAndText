d31328e70f5aa31f331de2a35b94a1eafd900084
pmvera <pablomvera14@gmail.com>
2019-10-06 12:40:39 +0200

    Modulate the code

--- speech2text.py
+++ speech2text.py
@@ -3,30 +3,33 @@ import sys
 
 class SpeechToText:
     def __init__(self, file=""):
-        r = sr.Recognizer()
+        self.r = sr.Recognizer()
         if (file == ""):
             mic = sr.Microphone()
             with mic as source:
                 print("Say something in spanish")
-                r.adjust_for_ambient_noise(source)
-                audio = r.listen(source)
+                self.r.adjust_for_ambient_noise(source)
+                self.audio = self.r.listen(source)
                 print("Listened")
         else:
             src = sr.AudioFile(file)
             with src as source:
                 print("Recording audio file")
-                audio = r.record(source)
+                self.audio = self.r.record(source)
                 print("Recorded")
 
+    def display_audio(self):
         language = raw_input("In wich lenguage is the audio?(English/Spanish) ")
 
         if language == "Spanish":
-            print("Text: " + r.recognize_google(audio, language="es-ES"))
+            print("Text: " + self.r.recognize_google(self.audio, language="es-ES"))
         else:
-            print("Text: " + r.recognize_google(audio, language="en-EN"))
+            print("Text: " + self.r.recognize_google(self.audio, language="en-EN"))
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         StT = SpeechToText(sys.argv[1])
     else:
         stT = SpeechToText()
+
+    stT.display_audio()
